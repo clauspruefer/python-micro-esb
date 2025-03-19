306f9a5b604f888b0604e176e346d4eea30b53c4
Claus Prüfer <pruefer@webcodex.de>
2025-03-19 20:10:53 +0100

    Correct wrong version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
 
     name = 'microesb',
-    version = '0.8rc1',
+    version = '1.0rc1',
     author = 'Claus Prüfer',
     author_email = 'pruefer@webcodex.de',
     description = 'A small OOP based Enterprise Service Bus implementation.',
