1e1274ba0e7e9d9ec3d015c5f264682b71386420
Claus Prüfer <pruefer@webcodex.de>
2025-04-29 10:42:32 +0200

    Update version to 1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup
 setup(
 
     name = 'microesb',
-    version = '1.0rc2',
     author = 'Claus Prüfer',
     author_email = 'pruefer@webcodex.de',
     description = 'A small OOP based Enterprise Service Bus implementation.',
