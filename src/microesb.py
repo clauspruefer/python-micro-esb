d09a4875537be556a79a1512ea626fde27c3b8e4
Claus Prüfer <pruefer@webcodex.de>
2025-04-29 13:02:31 +0200

    TODOs not in docstrings!

--- src/microesb.py
+++ src/microesb.py
@@ -47,7 +47,7 @@ class BaseHandler(JSONTransformer, metaclass=abc.ABCMeta):
         :ivar classref logger: logging logger reference
         :ivar dict[SYSProperties] _SYSProperties: internal properties processing dict
         :ivar classref _SYSParentObject: internal (hierarchical) class instance ref
-        :ivar list[classref] _SYSClassNames: internal class refs dict (should be renamed to _SYSClassRefs)
+        :ivar list[classref] _SYSClassNames: internal class refs dict
         """
 
         self.logger = logging.getLogger(__name__)
