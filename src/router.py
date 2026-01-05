3c540c1b27f738588811f885f9f687dc4a1b2817
Claus Prüfer <pruefer@webcodex.de>
2026-01-05 07:23:12 +0100

    Add docstrings

--- src/router.py
+++ src/router.py
@@ -16,8 +16,19 @@ mod_ref = importlib.import_module('user_routing')
 
 
 class ServiceRouter():
+    """ ServiceRouter class.
+    """
 
     def send(self, send_id, metadata):
+        """ send() method.
+
+        :param str send_id: service method id
+        :param dynamic metadata: first argument passed to service method function
+        :rtype: dict | None
+
+        Execute method with given id in `send_id` from imported user_routing.py module
+        and return result dict or None.
+        """
         logger.info('ServiceRouter send_id:{} metadata:{}'.format(send_id, metadata))
         func_ref = getattr(mod_ref, send_id)
         logger.debug('FuncRef:{}'.format(func_ref))
